2e826f77cc7b7d375521c022ebc32fe606abf0c5
hainguyen <hainguyen56211@gmail.com>
2022-05-08 11:47:14 +0700

    fix layout

--- 5_NNHaiLDHaiPDDungNQHoanNDHa_LTNET/frm_giaodienchinh_ha.Designer.cs
+++ 5_NNHaiLDHaiPDDungNQHoanNDHa_LTNET/frm_giaodienchinh_ha.Designer.cs
@@ -45,7 +45,8 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(378, 31);
+            this.label1.Location = new System.Drawing.Point(284, 25);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(432, 31);
             this.label1.TabIndex = 0;
@@ -57,9 +58,10 @@
             this.btn_qlnhanvien_ha.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn_qlnhanvien_ha.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn_qlnhanvien_ha.ForeColor = System.Drawing.Color.ForestGreen;
-            this.btn_qlnhanvien_ha.Location = new System.Drawing.Point(41, 181);
+            this.btn_qlnhanvien_ha.Location = new System.Drawing.Point(51, 147);
+            this.btn_qlnhanvien_ha.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btn_qlnhanvien_ha.Name = "btn_qlnhanvien_ha";
-            this.btn_qlnhanvien_ha.Size = new System.Drawing.Size(212, 38);
+            this.btn_qlnhanvien_ha.Size = new System.Drawing.Size(171, 31);
             this.btn_qlnhanvien_ha.TabIndex = 1;
             this.btn_qlnhanvien_ha.Text = "Quản Lý Nhân Viên";
             this.btn_qlnhanvien_ha.UseVisualStyleBackColor = false;
@@ -69,9 +71,10 @@
             this.btn_qlmathang_ha.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn_qlmathang_ha.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn_qlmathang_ha.ForeColor = System.Drawing.Color.Red;
-            this.btn_qlmathang_ha.Location = new System.Drawing.Point(41, 254);
+            this.btn_qlmathang_ha.Location = new System.Drawing.Point(51, 206);
+            this.btn_qlmathang_ha.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btn_qlmathang_ha.Name = "btn_qlmathang_ha";
-            this.btn_qlmathang_ha.Size = new System.Drawing.Size(212, 38);
+            this.btn_qlmathang_ha.Size = new System.Drawing.Size(171, 31);
             this.btn_qlmathang_ha.TabIndex = 2;
             this.btn_qlmathang_ha.Text = "Quản Lý Mặt Hàng";
             this.btn_qlmathang_ha.UseVisualStyleBackColor = true;
@@ -81,9 +84,10 @@
             this.btn_qlloaihang_ha.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn_qlloaihang_ha.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn_qlloaihang_ha.ForeColor = System.Drawing.Color.DodgerBlue;
-            this.btn_qlloaihang_ha.Location = new System.Drawing.Point(41, 327);
+            this.btn_qlloaihang_ha.Location = new System.Drawing.Point(51, 266);
+            this.btn_qlloaihang_ha.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btn_qlloaihang_ha.Name = "btn_qlloaihang_ha";
-            this.btn_qlloaihang_ha.Size = new System.Drawing.Size(212, 38);
+            this.btn_qlloaihang_ha.Size = new System.Drawing.Size(171, 31);
             this.btn_qlloaihang_ha.TabIndex = 3;
             this.btn_qlloaihang_ha.Text = "Quản Lý Loại Hàng";
             this.btn_qlloaihang_ha.UseVisualStyleBackColor = true;
@@ -93,9 +97,10 @@
             this.btn_qldoanhthu_ha.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn_qldoanhthu_ha.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn_qldoanhthu_ha.ForeColor = System.Drawing.Color.Coral;
-            this.btn_qldoanhthu_ha.Location = new System.Drawing.Point(41, 400);
+            this.btn_qldoanhthu_ha.Location = new System.Drawing.Point(51, 325);
+            this.btn_qldoanhthu_ha.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btn_qldoanhthu_ha.Name = "btn_qldoanhthu_ha";
-            this.btn_qldoanhthu_ha.Size = new System.Drawing.Size(212, 38);
+            this.btn_qldoanhthu_ha.Size = new System.Drawing.Size(171, 31);
             this.btn_qldoanhthu_ha.TabIndex = 6;
             this.btn_qldoanhthu_ha.Text = "Quản Lý Doanh Thu";
             this.btn_qldoanhthu_ha.UseVisualStyleBackColor = true;
@@ -105,9 +110,10 @@
             this.btn_qlkhachhang_ha.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn_qlkhachhang_ha.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn_qlkhachhang_ha.ForeColor = System.Drawing.Color.DarkMagenta;
-            this.btn_qlkhachhang_ha.Location = new System.Drawing.Point(41, 473);
+            this.btn_qlkhachhang_ha.Location = new System.Drawing.Point(51, 384);
+            this.btn_qlkhachhang_ha.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btn_qlkhachhang_ha.Name = "btn_qlkhachhang_ha";
-            this.btn_qlkhachhang_ha.Size = new System.Drawing.Size(212, 38);
+            this.btn_qlkhachhang_ha.Size = new System.Drawing.Size(171, 31);
             this.btn_qlkhachhang_ha.TabIndex = 5;
             this.btn_qlkhachhang_ha.Text = "Quản Lý Khách Hàng";
             this.btn_qlkhachhang_ha.UseVisualStyleBackColor = true;
@@ -117,9 +123,10 @@
             this.btn_muahang_ha.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn_muahang_ha.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn_muahang_ha.ForeColor = System.Drawing.Color.SlateBlue;
-            this.btn_muahang_ha.Location = new System.Drawing.Point(261, 181);
+            this.btn_muahang_ha.Location = new System.Drawing.Point(245, 147);
+            this.btn_muahang_ha.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btn_muahang_ha.Name = "btn_muahang_ha";
-            this.btn_muahang_ha.Size = new System.Drawing.Size(212, 38);
+            this.btn_muahang_ha.Size = new System.Drawing.Size(171, 31);
             this.btn_muahang_ha.TabIndex = 4;
             this.btn_muahang_ha.Text = "Mua Hàng";
             this.btn_muahang_ha.UseVisualStyleBackColor = true;
@@ -127,9 +134,10 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(492, 102);
+            this.pictureBox1.Location = new System.Drawing.Point(420, 84);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(666, 515);
+            this.pictureBox1.Size = new System.Drawing.Size(646, 491);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
             this.pictureBox1.TabIndex = 7;
             this.pictureBox1.TabStop = false;
@@ -138,9 +146,10 @@
             // 
             this.btn_gttv_ha.BackColor = System.Drawing.Color.Moccasin;
             this.btn_gttv_ha.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btn_gttv_ha.Location = new System.Drawing.Point(1060, 623);
+            this.btn_gttv_ha.Location = new System.Drawing.Point(983, 579);
+            this.btn_gttv_ha.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btn_gttv_ha.Name = "btn_gttv_ha";
-            this.btn_gttv_ha.Size = new System.Drawing.Size(98, 30);
+            this.btn_gttv_ha.Size = new System.Drawing.Size(74, 24);
             this.btn_gttv_ha.TabIndex = 4;
             this.btn_gttv_ha.Text = "about";
             this.btn_gttv_ha.UseVisualStyleBackColor = false;
@@ -148,10 +157,10 @@
             // 
             // frm_giaodienchinh_ha
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
-            this.ClientSize = new System.Drawing.Size(1230, 705);
+            this.ClientSize = new System.Drawing.Size(1068, 614);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.btn_qldoanhthu_ha);
             this.Controls.Add(this.btn_qlkhachhang_ha);
@@ -161,6 +170,7 @@
             this.Controls.Add(this.btn_qlmathang_ha);
             this.Controls.Add(this.btn_qlnhanvien_ha);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "frm_giaodienchinh_ha";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form Giao diện chính Duy Hà";
